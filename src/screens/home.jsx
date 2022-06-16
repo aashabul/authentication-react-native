f594481fcea016e6c1e248304c75c41ed2f0f45b
aashabul <aashabulimam@gmail.com>
2022-06-16 22:11:53 +0600

    flatlist error solved

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,28 +1,79 @@
-import { View, Text, SafeAreaView, StyleSheet, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SafeViewAndroid from "../theme/globalStyle";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  FlatList,
+} from "react-native";
+
 import { AntDesign } from "@expo/vector-icons";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../firebase/firebase.config";
+
+const Item = ({ title, color, description }) => (
+  <Pressable
+    style={{
+      backgroundColor: color,
+      marginBottom: 25,
+      borderRadius: 16,
+      padding: 15,
+    }}
+  >
+    <Text style={{ fontSize: 24, color: "white" }}>{title}</Text>
+  </Pressable>
+);
 
 export default function Home({ navigation, route, user }) {
   // console.log("user-->", user);
+  const [notes, setNotes] = useState([]);
+
+  //fetch notes collection
+  useEffect(() => {
+    const q = query(collection(db, "notes"), where("uid", "==", user.uid));
+    const notesListenerSubscription = onSnapshot(q, (querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+      setNotes(list);
+    });
+    return notesListenerSubscription;
+  }, []);
+  // console.log("notes-->", notes);
+
+  const renderItem = ({ item }) => (
+    <Item
+      title={item.title}
+      color={item.color}
+      description={item.description}
+    />
+  );
+
   const onPressCreate = () => {
     navigation.navigate("Create");
   };
   return (
-    <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.conainer]}>
+    <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea]}>
       <View style={styles.noteContainer}>
         <Text>my notes</Text>
         <Pressable onPress={onPressCreate}>
           <AntDesign name="pluscircleo" size={24} color="black" />
         </Pressable>
       </View>
+
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.title}
+        contentContainerStyle={{ padding: 20 }}
+      />
     </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
-  conainer: {
-    flex: 1,
-  },
   noteContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
